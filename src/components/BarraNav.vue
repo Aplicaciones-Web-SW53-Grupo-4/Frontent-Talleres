63e3b4a43386b77f0a1fe8aab63997f44baf62b0
Mauricio Salas <119011860+mauricio0801@users.noreply.github.com>
2023-09-01 21:30:25 -0500

    se agrego el boton Contactanos

--- src/components/BarraNav.vue
+++ src/components/BarraNav.vue
@@ -3,35 +3,37 @@ import Beneficios from './Beneficios.vue';
 import Inicio from './Inicio.vue';
 import Nosotros from './Nosotros.vue';
 import AcercaDe from './AcercaDe.vue';
+import Contactanos from "@/components/Contactanos.vue";
 
 </script>
 <template>
-    <div  class="container2" >
-        <Inicio/>
-        <Nosotros />
-        <Beneficios/>
-        <AcercaDe/>
-    </div>
+  <div  class="container2" >
+    <Inicio/>
+    <Nosotros />
+    <Beneficios/>
+    <AcercaDe/>
+    <Contactanos/>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "BarraNav",
-    components: { Inicio, Nosotros },
+  name: "BarraNav",
+  components: { Inicio, Nosotros, Beneficios, AcercaDe, Contactanos },
 }
 </script>
 
 <style scoped>
 .container2 {
-    width:100%;
-    display: flex;
-    flex-direction: row;
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100px;
-    background-color: aqua;
-    
+  width:100%;
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100px;
+  background-color: aqua;
+
 }
 
 </style>
